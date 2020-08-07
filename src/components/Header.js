24749a9746be397f8cbeefaeb4ac1f7bc76c1927
tombo gokuraku <tombo.gokuraku.1040@gmail.com>
2020-08-07 14:32:38 +0900

    fix: Increase the contrast to solve the A11y problem

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import tw, { styled } from "twin.macro"
 
 const Header = styled.header(() => [
-  tw`sticky top-0 px-6 py-2 mb-6 bg-primary-400`,
+  tw`sticky top-0 px-6 py-2 mb-6 bg-primary-600`,
 ])
 
 export default Header
